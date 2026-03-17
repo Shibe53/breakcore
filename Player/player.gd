7edd4e94111d378a7cc1fd0e22e0e4926ddc3db6
Shibe53 <raulserban05@gmail.com>
2026-03-17 15:07:59 +0100

    Stats addition

--- Player/player.gd
+++ Player/player.gd
@@ -1,16 +1,18 @@
 extends CharacterBody2D
 class_name Player
 
+@onready var hurtbox = $Hurtbox
+
 @export var ACCELERATION = 800
 @export var MAX_SPEED = 200
 @export var FRICTION = 500
-
 @export var controls : Resource = null
 
 enum {
 	MOVE
 }
 
+var stats = PlayerStats
 var state = MOVE
 
 func _ready():
@@ -33,3 +35,16 @@ func move_state(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	move_and_slide()
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	if not hurtbox.invincible:
+		stats.health -= area.damage
+		var knockback_direction = area.owner.position.direction_to(position)
+		velocity = knockback_direction * 200
+		hurtbox.start_invincibility(0.6)
+
+func _on_hurtbox_invincibility_ended() -> void:
+	pass
+
+func _on_hurtbox_invincibility_started() -> void:
+	pass
